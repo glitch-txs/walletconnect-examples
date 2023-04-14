85c1a89ea8276d7cdb896c89ec0338373e4c4f36
Glitch <glitch.txs@gmail.com>
2023-04-14 17:21:39 -0300

    initial examples boilerplate

---
+++ nextjs/web3modal/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Web3Button, Web3NetworkSwitch } from "@web3modal/react";
+import dynamic from "next/dynamic";
+
+// Read: https://github.com/wagmi-dev/wagmi/issues/542#issuecomment-1144178142
+const ConnectButton = dynamic(() => import("@/common/customButton"), {
+  ssr: false,
+})
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <ConnectButton/>
+      <br/>
+      <Web3Button avatar="show" icon="show" label="Connect Wallet" balance="show" />
+      <br/>
+      <Web3NetworkSwitch />
+    </main>
+  )
+}
